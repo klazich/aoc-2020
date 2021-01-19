f9aef2d64b14e43421a6aed94d2baa7ec9d92f00
Kevin Lazich <dev@kevinlazich.com>
2021-01-19 12:07:36 -0500

    Complete: Day 20

--- src/day20/part-two.test.js
+++
@@ -1,8 +0,0 @@
-import { parse } from './index'
-import runnerPartTwo, { input, expected } from './part-two'
-
-describe('Day 20', () => {
-  test('Part Two', () => {
-    expect(runnerPartTwo(parse(input))).toBe(expected)
-  })
-})
